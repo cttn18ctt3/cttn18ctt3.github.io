ed4a184b052ce153298cd11f4634fc49cdbb477d
Duy Master <viplazylmht@gmail.com>
2020-02-27 23:46:05 +0700

    NavBar on air (#3)
    * Import frontend code
    
    * Chinh sua linh tinh
    
    * Them thong bao
    
    * them css history
    
    * hieu map
    
    * FIX NAV BAR, removed user's options
    
    * Add custom navbar to history.html
    Fix navbar in all page
    make POST request in khosach.html instead of GET request
    
    Co-authored-by: PLHieu <45454408+PLHieu@users.noreply.github.com>

--- script/khosach-script.js
+++ script/khosach-script.js
@@ -1,20 +1,16 @@
 $(document).ready(function() {
+        // cho modal box
+        $('.momo').click(function(e) {
+            e.preventDefault();
+            $('.momo').removeClass('hienra');
+            $('.modal_box').removeClass('modal_show');
+        });
 
-
-
-    // cho modal box
-    $('.momo').click(function(e) {
-        e.preventDefault();
-        $('.momo').removeClass('hienra');
-        $('.modal_box').removeClass('modal_show');
-    });
-
-    $('.thongtin-btn').click(function(e) {
-        e.preventDefault();
-        $('.momo').addClass('hienra');
-        $('.modal_box').addClass('modal_show');
-        console.log("s");
-    });
-
-  
+        $('.thongtin-btn').click(function(e) {
+            console.log("hieu map");
+            e.preventDefault();
+            $('.momo').addClass('hienra');
+            $('.modal_box').addClass('modal_show');
+        });
+      
 });
