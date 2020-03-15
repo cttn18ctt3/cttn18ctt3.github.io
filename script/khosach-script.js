f43907f5f593b061b239967724f5cd2f26732a40
PLHieu <hieuflong@gmail.com>
2020-03-15 23:10:59 +0700

    callback khosach, fix anh muon sach, checkbox muonsach

--- script/khosach-script.js
+++ script/khosach-script.js
@@ -1,24 +1,19 @@
 $(function(){
- 	
-    hieuungload = new TimelineMax({onComplete:noidungvaodi}); 
+     
+    
+
+    hieuungload = new TimelineMax({}); 
     hieuungload
-    .from($('.loading'),0.4,{opacity:0})
+    .from($('.loading'),0.7,{opacity:0})
     .from($('.khoiload'),0.7,{scale:2,opacity:0})
 
     //lap lai
-    .to($('.khoiload'),0.8,{scale:0.3,ease:Power4.easeOut})
-    .to($('.khoiload'),0.8,{scale:1,ease: Elastic.easeOut.config(1, 0.3)})
-   .to($('.khoiload'),0.8,{scale:0.3,ease:Power4.easeOut})
-    .to($('.khoiload'),0.8,{scale:1,ease: Elastic.easeOut.config(1, 0.3)})
+    .to($('.khoiload'),1,{scale:0.3,ease:Power4.easeOut})
+    .to($('.khoiload'),1,{scale:1,ease: Elastic.easeOut.config(1, 0.3)})
+  
 
     //ket thuc 
     .to($('.khoiload'),0.8,{scale:7,opacity:0,ease:Power4.easeOut})
     .to($('.loading'),0.8,{x:-2200,ease:Power1.easeOut})
-     hieuungnoidung = new TimelineMax({paused:true}); 
-
-    function noidungvaodi()
-    {
-        hieuungnoidung.play();
-    }
-
+     
 })  
