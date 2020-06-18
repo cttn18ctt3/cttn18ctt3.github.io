300d4795d4331b3a6bf441470e8e3c31044cd70b
viplazylmht <viplazylmht@gmail.com>
2020-06-19 01:05:52 +0700

    fix alot of thing remove history.html

--- script/allpage.js
+++ script/allpage.js
@@ -19,3 +19,38 @@ $(document).ready(function() {
     $("#cuoitrang").load("footer.html"); 
 
 });
+
+function dongydieukhoan() {
+    // Get the checkbox
+    var checkBox = document.getElementById("agree");
+
+    // If the checkbox is checked, display the button
+    var button = document.getElementById("submit-form");
+    if (checkBox.checked == true){
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+}
+
+function parse_query_string(query) {
+    var vars = query.split("&");
+    var query_string = {};
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        var key = decodeURIComponent(pair[0]);
+        var value = decodeURIComponent(pair[1]);
+        // If first entry with this name
+        if (typeof query_string[key] === "undefined") {
+            query_string[key] = decodeURIComponent(value);
+            // If second entry with this name
+        } else if (typeof query_string[key] === "string") {
+            var arr = [query_string[key], decodeURIComponent(value)];
+            query_string[key] = arr;
+            // If third or later entry with this name
+        } else {
+            query_string[key].push(decodeURIComponent(value));
+        }
+    }
+    return query_string;
+}
