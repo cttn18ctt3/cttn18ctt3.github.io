592c03245b328843a9ee276cc6d4dcccae8db29c
Duy Master <viplazylmht@gmail.com>
2020-06-25 23:30:15 +0700

    add cookie and new layout when donate button is clicked

--- script/allpage.js
+++ script/allpage.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // them thanh nav vo dau trang
-    $("#dautrang").load("nav.html"); 
+    $("#dautrang").load("nav.html");
 
     var body = document.documentElement.scrollTop;
-    $(window).scroll(function() {
+    $(window).scroll(function () {
 
         body = document.documentElement.scrollTop;
         if (body > 0) {
@@ -16,7 +16,7 @@ $(document).ready(function() {
     });
 
     // them phan footer vo cuoi trang
-    $("#cuoitrang").load("footer.html"); 
+    $("#cuoitrang").load("footer.html");
 
 });
 
@@ -26,7 +26,7 @@ function dongydieukhoan() {
 
     // If the checkbox is checked, display the button
     var button = document.getElementById("submit-form");
-    if (checkBox.checked == true){
+    if (checkBox.checked == true) {
         button.disabled = false;
     } else {
         button.disabled = true;
@@ -53,4 +53,53 @@ function parse_query_string(query) {
         }
     }
     return query_string;
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function removeCookie(cname) {
+    var d = new Date(0);
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=;" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function saveCookie() {
+    var formData = $('form#test-form');
+    var arrayInput = formData.serializeArray();
+
+    var list = ["name", "donator_id", "email", "phone", "address"]
+    for (var i in arrayInput) {
+        if (list.includes(arrayInput[i].name)) setCookie(arrayInput[i].name, arrayInput[i].value, 365);
+        if (arrayInput[i].name == "donator_id") setCookie("id_user", arrayInput[i].value, 365)
+        if (arrayInput[i].name == "id_user") setCookie("donator_id", arrayInput[i].value, 365)
+    }
+}
+
+function loadCookie() {
+    $('input#name')[0].value = getCookie("name");
+    if ($('input#id_user').length != 0) $('input#id_user')[0].value = getCookie("id_user");
+    if ($('input#donator_id').length != 0) $('input#donator_id')[0].value = getCookie("donator_id");
+    $('input#email')[0].value = getCookie("email");
+    $('input#phone')[0].value = getCookie("phone");
+    $('textarea#address')[0].value = getCookie("address");
 }
